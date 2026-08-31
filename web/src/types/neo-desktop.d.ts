42f78682d241a0f8ffabd14486af2b998033c890
NeoZJXisme <95279527gm@gmail.com>
2026-08-31 15:54:30 +0800

    feat: type NZX desktop bridge

---
+++ web/src/types/neo-desktop.d.ts
@@ -0,0 +1,26 @@
+export {};
+
+type NeoNzxPayload = {
+    path: string;
+    fileName: string;
+    bytes: Uint8Array | ArrayBuffer;
+};
+
+type NeoNzxSaveResult = {
+    canceled: boolean;
+    path?: string;
+    fileName?: string;
+};
+
+declare global {
+    interface Window {
+        neoDesktop?: {
+            isDesktop: true;
+            saveNzx: (payload: { bytes: Uint8Array | ArrayBuffer; suggestedName: string; saveAs?: boolean }) => Promise<NeoNzxSaveResult>;
+            openNzxDialog: () => Promise<NeoNzxPayload | null>;
+            takePendingNzx: () => Promise<NeoNzxPayload | null>;
+            onOpenNzx: (listener: (payload: NeoNzxPayload) => void) => () => void;
+            clearNzxPath: () => Promise<boolean>;
+        };
+    }
+}
